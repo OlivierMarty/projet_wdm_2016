a117f41091fc2088f6056fd0c993a3c094a46ee6
Olivier Marty <olivier.marty.m@gmail.com>
2016-02-08 21:02:32 +0100

    Exemple traffic ligne 7

--- ratp.sh
+++ ratp.sh
@@ -8,7 +8,9 @@ function ratp_url_metro {
 }
 
 # requete prochains passages
-xquery_html -q:query_ratp_horaires_metro -s:"$(ratp_url_metro "republique" 5 A)"
+#xquery_html -q:query_ratp_horaires_metro -s:"$(ratp_url_metro "republique" 5 A)"
 
 # requete traffic
-xquery_html -q:query_ratp_traffic -s:"http://www.ratp.fr/meteo/"
+traffic=$(xquery_html -q:query_ratp_traffic -s:"http://www.ratp.fr/meteo/")
+# traffic metro 7
+xquery -qs:"//ligne[@id='ligne_metro_7']" -s:- <<< $traffic
