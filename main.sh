94b23ca1538ca14729f422ba1a4ad9b0fe4aae54
Olivier Marty <olivier.marty.m@gmail.com>
2016-02-08 21:58:07 +0100

    Fix false à la place de true

--- main.sh
+++ main.sh
@@ -7,7 +7,7 @@ source ./notification.sh
 traffic=$(xquery_html -q:query_ratp_traffic -s:"http://www.ratp.fr/meteo/")
 traffic_ligne_7=$(xquery -qs:"//ligne[@id='ligne_metro_7']" -s:- <<< "$traffic")
 statut=$(xquery -qs:"/ligne/normal/text()" -s:- \!omit-xml-declaration=yes <<< "$traffic_ligne_7")
-if [ "$statut" = "true" ]
+if [ "$statut" = "false" ]
 then
   message=$(xquery -qs:"/ligne/message/text()" -s:- \!omit-xml-declaration=yes <<< "$traffic_ligne_7")
   notify "Problème sur la ligne 7 : $message"
