38ca8a17a545de1cab2de2f44aeba5a710b30c42
Olivier Marty <olivier.marty.m@gmail.com>
2016-02-08 20:50:38 +0100

    Ajoute ratp traffic

--- ratp.sh
+++ ratp.sh
@@ -7,5 +7,8 @@ function ratp_url_metro {
   echo "http://www.ratp.fr/horaires/fr/ratp/metro/prochains_passages/PP/"$1"/"$2"/"$3
 }
 
-# requete
+# requete prochains passages
 xquery_html -q:query_ratp_horaires_metro -s:"$(ratp_url_metro "republique" 5 A)"
+
+# requete traffic
+xquery_html -q:query_ratp_traffic -s:"http://www.ratp.fr/meteo/"
