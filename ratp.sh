ba9ae455a8bf34e216e3ac38cc9a04d7e00f6e0e
Olivier Marty <olivier.marty.m@gmail.com>
2016-02-08 21:49:46 +0100

    Utilisation de ratp + notification pour avertir d'un problème sur une ligne de métro

--- ratp.sh
+++ ratp.sh
@@ -7,10 +7,12 @@ function ratp_url_metro {
   echo "http://www.ratp.fr/horaires/fr/ratp/metro/prochains_passages/PP/"$1"/"$2"/"$3
 }
 
+# EXEMPLES :
+
 # requete prochains passages
 #xquery_html -q:query_ratp_horaires_metro -s:"$(ratp_url_metro "republique" 5 A)"
 
 # requete traffic
-traffic=$(xquery_html -q:query_ratp_traffic -s:"http://www.ratp.fr/meteo/")
+#traffic=$(xquery_html -q:query_ratp_traffic -s:"http://www.ratp.fr/meteo/")
 # traffic metro 7
-xquery -qs:"//ligne[@id='ligne_metro_7']" -s:- <<< "$traffic"
+#xquery -qs:"//ligne[@id='ligne_metro_7']" -s:- <<< "$traffic"
