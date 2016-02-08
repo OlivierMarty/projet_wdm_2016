e6f4483429487ab855456835d7a2d984a504edf9
Olivier Marty <olivier.marty.m@gmail.com>
2016-02-08 20:14:05 +0100

    Change xquery and xquery_html into functions : add indentation

--- conf.sh
+++ conf.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # xquery processor
-xquery="java -cp saxon.jar:tagsoup-1.2.jar net.sf.saxon.Query"
-xquery_html="java -cp saxon.jar:tagsoup-1.2.jar net.sf.saxon.Query -x:org.ccil.cowan.tagsoup.Parser"
+function xquery {
+  java -cp saxon.jar:tagsoup-1.2.jar net.sf.saxon.Query $@ \!indent=yes
+}
+function xquery_html {
+  java -cp saxon.jar:tagsoup-1.2.jar net.sf.saxon.Query -x:org.ccil.cowan.tagsoup.Parser $@ \!indent=yes
+}
