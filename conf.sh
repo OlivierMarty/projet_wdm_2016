2ed859af66ea73f7a0e461fdcc192448602b1dff
Olivier Marty <olivier.marty.m@gmail.com>
2016-02-08 21:23:25 +0100

    Ajoute notification.sh

--- conf.sh
+++ conf.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# NOTIFICATIONS
+notification_methode="free"
+
+# free sms api http://mobile.free.fr/
+notification_free_user=""
+notification_free_pass=""
+
 # xquery processor
 function xquery {
   java -cp saxon.jar:tagsoup-1.2.jar net.sf.saxon.Query $@ \!indent=yes
---
+++ notification.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source ./conf.sh
+
+# usage : notify "message"
+function notify {
+  case "$notification_methode" in
+    free)
+      curl "https://smsapi.free-mobile.fr/sendmsg"\
+        --get\
+        --data-urlencode user=$notification_free_user\
+        --data-urlencode pass=$notification_free_pass\
+        --data-urlencode msg="$@"
+      ;;
+    *)
+      echo "\$notification_methode=$notification_methode non reconnu !" >&2
+      exit 1
+  esac
+}
