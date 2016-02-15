43c77d0416e76c53d12ab86cffa0d5630d471b65
Olivier Marty <olivier.marty.m@gmail.com>
2016-02-15 13:40:08 +0100

    End plugin velib

--- velib.sh
+++ velib.sh
@@ -4,7 +4,6 @@ source ./conf.sh
 
 velib_url="http://opendata.paris.fr/api/records/1.0/search/?dataset=stations-velib-disponibilites-en-temps-reel&facet=banking&facet=status&refine.status=OPEN&rows=10" # TODO augmenter rows
 
-velib_json=$(wget "$velib_url" -O -)
+velib_json=$(wget "$velib_url" -O -) # piping does not seem to work... (python does not wait enough ?)
 velib_xml=$(./xml2json.py -t json2xml <<< "$velib_json")
-echo "$velib_xml"
-#xquery_html -q:query_velib -s:"$velib_xml"
+xquery -q:query_velib -s:- <<< "$velib_xml"
