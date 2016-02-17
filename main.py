e7879b66add37bfe41a6a962963559b6acedb012
Olivier Marty <olivier.marty.m@gmail.com>
2016-02-17 18:20:00 +0100

    Add jcdecaux_vls in python

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from class_xml import XML
-
+import config
 
 def ratp_traffic():
-   query = """
+  query = """
      for $ligne in //*:div[@class="encadre_ligne"]
        let $img := $ligne/*:img[1]
        let $a := $ligne//*:span[@class="perturb_link"]/*:a[1]
        return <result>
-           <script>ratp_traffic</script>
+           <source>ratp_traffic</source>
            <id>ratp_traffic_{ data($ligne/@id) }</id>
            <status>
            {
@@ -34,7 +34,39 @@ def ratp_traffic():
          </result>
      """
 
-   return XML(url="http://www.ratp.fr/meteo/").xquery(query)
+  return XML(url="http://www.ratp.fr/meteo/", lang="html").xquery(query)
+
+def jcdecaux_vls():
+  query = """
+     (: TODO bug de fuseau horaire :)
+     (:~ convert epoch seconds to dateTime :)
+     declare function local:millitimestamp-to-date($v) as xs:string
+     {
+       let $len := string-length($v)
+       let $timestamp := substring($v, 1, $len - 3)
+       let $datetime := xs:dateTime("1970-01-01T00:00:00-00:00") + xs:dayTimeDuration(concat("PT", $timestamp, "S"))
+       return format-dateTime($datetime, "à [H01]h[M01] le [D01]/[M01]", "en", "AD", "fr")
+     };
+     
+     for $res in /*:json/*:item
+       let $places := $res/*:available_bike_stands/text()
+       return <result>
+           <source>jcdecaux_vls</source>
+           <id>jcdecaux_vls_{ $res/*:number/text() }</id>
+           {
+             if ($places > 4)
+             then <status>ok</status>
+             else (
+                 <status>problem</status>,
+                 <message>Station vélo { lower-case($res/*:name/text()) } : { local:millitimestamp-to-date($res/*:last_update/text()) } : plus que { $places } places disponibles !</message>
+               )
+           }
+         </result>
+     """
+
+  return XML(url="https://api.jcdecaux.com/vls/v1/stations?contract=paris&apiKey="+config.api_key['jcdecaux'], lang="json").xquery(query)
 
 for x in ratp_traffic():
+  print(str(x))
+for x in jcdecaux_vls():
   print(str(x))
