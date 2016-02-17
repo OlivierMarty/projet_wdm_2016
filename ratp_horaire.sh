c26986d26d6710470ced938b6035f320976b435f
Olivier Marty <olivier.marty.m@gmail.com>
2016-02-17 18:53:20 +0100

    Remove bash files

--- ratp_horaire.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# usage : ratp_url_metro arret n°ligne A|R
-function ratp_url_metro {
-  echo "http://www.ratp.fr/horaires/fr/ratp/metro/prochains_passages/PP/"$1"/"$2"/"$3
-}
-
-# EXEMPLE : requete prochains passages
-#xquery_html -q:query_ratp_horaires_metro -s:"$(ratp_url_metro "republique" 5 A)"
--- ratp_traffic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ./conf.sh
-
-xquery_html -q:query_ratp_traffic -s:"http://www.ratp.fr/meteo/"
