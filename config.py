34f68e25a99bb80cd6266c685c20e8edeac908d6
Olivier Marty <olivier.marty.m@gmail.com>
2016-02-17 18:24:56 +0100

    Add notification and events in config.py

--- config.py
+++ config.py
@@ -3,3 +3,25 @@
 api_key = {
   'jcdecaux' : ''
 }
+
+
+# EVENEMENTS A SURVEILLER
+
+events = {
+  'ratp_traffic' : ['ligne_metro_7'],
+  'jcdecaux_vls' : ['42703']
+}
+
+# NOTIFICATIONS
+
+notification = {
+  'method' : 'print', # ou "free" ou "sendmail"
+  'free' : { # méthode "free" (sms api voir http://mobile.free.fr/ )
+    'user' : '123456789',
+    'pass' : 'wwwwwwwww'
+  },
+  'sendmail' : { # méthode "sendmail"
+    'to' : 'example@example.com',
+    'object' : 'Notification traffic'
+  }
+}
